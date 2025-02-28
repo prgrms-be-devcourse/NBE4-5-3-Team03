452fc6f99a7a83a10cc83574e30efb4f7e5b6edf
haeun9988 <haeun9988@naver.com>
2025-02-28 16:57:49 +0900

    first

---
+++ src/test/java/com/example/Flicktionary/FlicktionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Flicktionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlicktionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
