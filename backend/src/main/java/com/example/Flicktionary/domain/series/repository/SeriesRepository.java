bde3bd736aa6f99cdc2b75243fba471cf5fd2964
JunKiBeom <dona0408@gmail.com>
2025-03-04 17:45:52 +0900

    fix: SeriesRepository 수정

--- backend/src/main/java/com/example/Flicktionary/domain/series/repository/SeriesRepository.java
+++ backend/src/main/java/com/example/Flicktionary/domain/series/repository/SeriesRepository.java
@@ -3,5 +3,5 @@ package com.example.Flicktionary.domain.series.repository;
 import com.example.Flicktionary.domain.series.domain.Series;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeriesRepository extends JpaRepository<Series, Integer> {
+public interface SeriesRepository extends JpaRepository<Series, Long> {
 }
