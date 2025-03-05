c0597899fe7bc0f09a3bbb5107c04e782a410405
jin214930 <jin214930@naver.com>
2025-03-05 10:31:41 +0900

    feat: 영화 목록 조회 구현

---
+++ backend/src/main/java/com/example/Flicktionary/domain/movie/dto/MovieResponse.java
@@ -0,0 +1,19 @@
+package com.example.Flicktionary.domain.movie.dto;
+
+import com.example.Flicktionary.domain.movie.entity.Movie;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MovieResponse {
+    private final long id;
+    private final long tmdbId;
+    private final String title;
+    private final String posterPath;
+    private final float average_rating;
+
+    public MovieResponse(Movie movie) {
+        this(movie.getId(), movie.getTmdbId(), movie.getTitle(), movie.getPosterPath(), movie.getAverageRating());
+    }
+}
--- backend/src/main/java/com/example/Flicktionary/domain/movie/service/MovieService.java
+++ backend/src/main/java/com/example/Flicktionary/domain/movie/service/MovieService.java
@@ -5,6 +5,10 @@ import com.example.Flicktionary.domain.movie.entity.Movie;
 import com.example.Flicktionary.domain.movie.repository.MovieRepository;
 import com.example.Flicktionary.domain.tmdb.service.TmdbService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,5 +42,22 @@ public class MovieService {
             movie.setReleaseDate(LocalDate.parse(movieDto.releaseDate()));
         }
         movie.setPosterPath(movieDto.posterPath());
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Movie> getMovies(String keyword, int page, int pageSize, String sortBy) {
+        Sort sort;
+
+        if (sortBy.equalsIgnoreCase("id")) {
+            sort = Sort.by(Sort.Direction.ASC, "id");
+        } else if (sortBy.equalsIgnoreCase("rating")) {
+            sort = Sort.by(Sort.Direction.DESC, "averageRating");
+        } else {
+            throw new RuntimeException("잘못된 정렬기준입니다.");
+        }
+
+        Pageable pageable = PageRequest.of(page - 1, pageSize, sort);
+
+        return movieRepository.findByTitleLike(keyword, pageable);
     }
 }
