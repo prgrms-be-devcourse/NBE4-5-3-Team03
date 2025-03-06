edb29b4b4831359cfc9c29f2c90a2542c5e1b4f7
haeun9988 <haeun9988@naver.com>
2025-03-06 21:21:54 +0900

    feat: 리뷰 entity, service, controller, dto, repository 등 수정 및 보강

--- backend/src/main/java/com/example/Flicktionary/domain/review/entity/Review.java
+++ backend/src/main/java/com/example/Flicktionary/domain/review/entity/Review.java
@@ -23,7 +23,7 @@ public class Review {
     // 유저 id (외래키)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
-    private UserAccount userAccount;
+    private User user;
 
     // 영화 id (외래키)
     @ManyToOne(fetch = FetchType.LAZY)
