6c23e2ca9127014e99a5eb307833f9947bbc25ee
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-03-07 18:02:37 +0900

    feature: 인증 실패시 발생하는 예외 핸들러 구현

--- backend/src/main/java/com/example/Flicktionary/global/security/SecurityConfig.java
+++ backend/src/main/java/com/example/Flicktionary/global/security/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.example.Flicktionary.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -23,11 +25,33 @@ public class SecurityConfig {
                         authorizeHttpRequests
                                 .anyRequest()
                                 .permitAll())
+                // CSRF 보호 비활성
                 .csrf(AbstractHttpConfigurer::disable)
+                // XSS 보호 비활성
                 .headers(AbstractHttpConfigurer::disable)
+                // Spring Security 세션 비활성 (JSESSIONID 쿠키 비생성)
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                // 커스텀 인증 필터 추가
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                // 인증 필터 예외 처리
+                .exceptionHandling((exceptionHandling) ->
+                        exceptionHandling
+                                .authenticationEntryPoint(
+                                        (request, response, authException) -> {
+                                            response.setContentType("application/json;charset=UTF-8");
+                                            response.setStatus(HttpStatus.FORBIDDEN.value());
+                                            response.getWriter().write("{\"message\": \"인증 토큰이 잘못되었거나 로그인이 필요합니다.\"}");
+                                        }
+                                )
+                                .accessDeniedHandler(
+                                        (request, response, authException) -> {
+                                            response.setContentType("application/json;charset=UTF-8");
+                                            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                                            response.getWriter().write("{\"message\": \"접근 권한이 없습니다.\"}");
+                                        }
+                                )
+                );
         return http.build();
     }
 }
