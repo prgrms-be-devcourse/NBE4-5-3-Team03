a4a0a2818d117991adc7bcbcfc5b506ed3b0cfe9
jin214930 <jin214930@naver.com>
2025-03-10 11:51:28 +0900

    chore: 수정

--- backend/src/main/java/com/example/Flicktionary/domain/movie/dto/MovieCastDto.java
+++ backend/src/main/java/com/example/Flicktionary/domain/movie/dto/MovieCastDto.java
@@ -4,10 +4,12 @@ import com.example.Flicktionary.domain.actor.dto.ActorDto;
 import com.example.Flicktionary.domain.movie.entity.MovieCast;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.lang.NonNull;
 
 @Getter
 @AllArgsConstructor
 public class MovieCastDto {
+    @NonNull
     private final ActorDto actor;
     private final String characterName;
 
