bfb320c71a1a2c2dbc43a1b484564b57ba0d1179
haeun9988 <haeun9988@naver.com>
2025-03-11 18:00:51 +0900

    fix: 롤백

--- frontend/src/app/movies/[id]/reviews/page.tsx
+++ frontend/src/app/movies/[id]/reviews/page.tsx
@@ -1,9 +1,16 @@
 import client from "@/lib/backend/client";
 import ClientPage from "./ClientPage";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { page?: string };
+}) {
   const { id } = params;
   const movie_id = parseInt(id, 10);
+
   // 기본 페이지는 0
   const page = searchParams.page ? parseInt(searchParams.page, 10) : 0;
 
@@ -11,7 +18,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   // 특정 영화의 리뷰 목록 가져오기
   const response = await client.GET("/api/reviews/movie/{movie_id}" as any, {
-    params: { path: { movie_id }, query: { page, size: 5 } },
+    params: { path: { movie_id } },
   });
 
   if (response.error) {
