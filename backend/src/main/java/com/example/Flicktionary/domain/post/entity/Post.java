0d8de710247f73753ffd11f4b128193013bd2c0f
haeun9988 <haeun9988@naver.com>
2025-03-21 16:30:10 +0900

    feat: 게시글 entity 제작

---
+++ backend/src/main/java/com/example/Flicktionary/domain/post/entity/Post.java
@@ -0,0 +1,51 @@
+package com.example.Flicktionary.domain.post.entity;
+
+import com.example.Flicktionary.domain.user.entity.UserAccount;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    // 게시글 id (기본키)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 유저 id (외래키)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserAccount userAccount;
+
+    // 게시글 제목
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    // 게시글 내용
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    // 게시글 생성 날짜
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    // 게시글 수정 날짜
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    // 게시글 스포일러 여부 토글
+    @Column(columnDefinition = "boolean default false")
+    private boolean isSpoiler;
+}
