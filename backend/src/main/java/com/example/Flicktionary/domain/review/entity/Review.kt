a85d43a35c7d7a9379b814618815af71e0c54b0a
haeun9988 <haeun9988@naver.com>
2025-04-04 21:53:45 +0900

    refactor: tdd 제외 코틀린화 완료

--- backend/src/main/java/com/example/Flicktionary/domain/review/entity/Review.kt
+++ backend/src/main/java/com/example/Flicktionary/domain/review/entity/Review.kt
@@ -1,45 +1,40 @@
-package com.example.Flicktionary.domain.review.entity;
+package com.example.Flicktionary.domain.review.entity
 
-import com.example.Flicktionary.domain.movie.entity.Movie;
-import com.example.Flicktionary.domain.series.entity.Series;
-import com.example.Flicktionary.domain.user.entity.UserAccount;
-import jakarta.persistence.*;
-import lombok.*;
+import com.example.Flicktionary.domain.movie.entity.Movie
+import com.example.Flicktionary.domain.series.entity.Series
+import com.example.Flicktionary.domain.user.entity.UserAccount
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "review")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class Review {
-
+data class Review(
     // 리뷰 id (기본키)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    var id: Long? = null,
 
     // 유저 id (외래키)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private UserAccount userAccount;
+    var userAccount: UserAccount? = null,
 
     // 영화 id (외래키)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "movie_id")
-    private Movie movie;
+    var movie: Movie? = null,
 
     // 드라마 id (외래키)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "series_id")
-    private Series series;
+    var series: Series? = null,
 
     // 평점
     @Column(nullable = false)
-    private int rating;
+    var rating: Int,
 
     // 리뷰
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String content;
+    var content: String
+) {
+    constructor() : this(null, null, null, null, 0, "")
 }
