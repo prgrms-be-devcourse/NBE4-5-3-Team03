0b95a56801ad6445c0203deba469879b5d21e829
JunKiBeom <dona0408@gmail.com>
2025-04-07 12:44:11 +0900

    refactor: ActorRepository .java to .kt

--- backend/src/main/java/com/example/Flicktionary/domain/actor/repository/ActorRepository.kt
+++ backend/src/main/java/com/example/Flicktionary/domain/actor/repository/ActorRepository.kt
@@ -1,12 +1,12 @@
-package com.example.Flicktionary.domain.actor.repository;
+package com.example.Flicktionary.domain.actor.repository
 
-import com.example.Flicktionary.domain.actor.entity.Actor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import com.example.Flicktionary.domain.actor.entity.Actor
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-public interface ActorRepository extends JpaRepository<Actor, Long> {
+interface ActorRepository : JpaRepository<Actor, Long> {
     @Query("SELECT a FROM Actor a WHERE LOWER(REPLACE(a.name, ' ', '')) LIKE CONCAT('%', :keyword, '%')")
-    Page<Actor> findByNameLike(String keyword, Pageable pageable);
+    fun findByNameLike(keyword: String, pageable: Pageable): Page<Actor>
 }
