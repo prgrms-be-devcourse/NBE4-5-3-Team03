deeb8ff32cd32fbe17004c257c085ea9e88d371b
JunKiBeom <dona0408@gmail.com>
2025-04-08 12:45:40 +0900

    refactor: GenreDto .java to .kt

--- backend/src/main/java/com/example/Flicktionary/domain/genre/dto/GenreDto.kt
+++ backend/src/main/java/com/example/Flicktionary/domain/genre/dto/GenreDto.kt
@@ -1,21 +1,6 @@
-package com.example.Flicktionary.domain.genre.dto;
+package com.example.Flicktionary.domain.genre.dto
 
-import com.example.Flicktionary.domain.genre.entity.Genre;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.lang.NonNull;
-
-@Getter
-@AllArgsConstructor
-public class GenreDto {
-    @NonNull
-    private final Long id;
-
-    @NonNull
-    private final String name;
-
-    public GenreDto(Genre genre) {
-        this.id = genre.getId();
-        this.name = genre.getName();
-    }
-}
+class GenreDto(
+    val id: Long,
+    val name: String
+)
