a3c79039e82bb5354d279a0277f28a54f548b5d1
JunKiBeom <dona0408@gmail.com>
2025-04-08 12:46:58 +0900

    refactor: GenreRepository .java to .kt

--- backend/src/main/java/com/example/Flicktionary/domain/genre/repository/GenreRepository.kt
+++ backend/src/main/java/com/example/Flicktionary/domain/genre/repository/GenreRepository.kt
@@ -1,10 +1,8 @@
-package com.example.Flicktionary.domain.genre.repository;
+package com.example.Flicktionary.domain.genre.repository
 
-import com.example.Flicktionary.domain.genre.entity.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.Flicktionary.domain.genre.entity.Genre
+import org.springframework.data.jpa.repository.JpaRepository
 
-import java.util.Optional;
-
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-    Optional<Genre> findByName(String genreName);
+interface GenreRepository : JpaRepository<Genre, Long> {
+    fun findByName(genreName: String): Genre?
 }
