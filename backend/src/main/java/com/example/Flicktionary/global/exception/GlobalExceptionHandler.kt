e9fccaaaf809c3f6f5690f98496e73eb05c62b4f
jin214930 <jin214930@naver.com>
2025-04-09 16:34:40 +0900

    validation 에러를 excetion handler에서 따로 처리하도록 수정

--- backend/src/main/java/com/example/Flicktionary/global/exception/GlobalExceptionHandler.kt
+++ backend/src/main/java/com/example/Flicktionary/global/exception/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.example.Flicktionary.global.exception
 import com.example.Flicktionary.global.dto.ResponseDto
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -17,7 +18,18 @@ class GlobalExceptionHandler {
     fun serviceExceptionHandler(e: ServiceException): ResponseEntity<ResponseDto<Nothing>> {
         return ResponseEntity
             .status(e.code)
-            .body(ResponseDto.of(e.code.toString(), e.message?: ""))
+            .body(ResponseDto.of(e.code.toString(), e.message ?: ""))
+    }
+
+    // Validation 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun methodArgumentNotValidExcetionHandler(e: MethodArgumentNotValidException): ResponseEntity<ResponseDto<Nothing>> {
+        val fieldError = e.bindingResult.fieldErrors.firstOrNull()
+        val errorMessage = fieldError?.defaultMessage ?: "유효하지 않은 요청입니다."
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ResponseDto.of(HttpStatus.BAD_REQUEST.toString(), errorMessage))
     }
 
     // 그 외 모든 예외 처리
