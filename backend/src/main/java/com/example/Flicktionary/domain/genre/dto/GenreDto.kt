300d85dff70f1773fb387709432e1e1659b74c02
jin214930 <jin214930@naver.com>
2025-04-09 12:20:56 +0900

    feat: 장르 검색 기능 구현

--- backend/src/main/java/com/example/Flicktionary/domain/genre/dto/GenreDto.kt
+++ backend/src/main/java/com/example/Flicktionary/domain/genre/dto/GenreDto.kt
@@ -1,6 +1,13 @@
 package com.example.Flicktionary.domain.genre.dto
 
-class GenreDto(
+import com.example.Flicktionary.domain.genre.entity.Genre
+
+data class GenreDto(
     val id: Long,
     val name: String
-)
+) {
+    constructor(genre: Genre) : this(
+        genre.id,
+        genre.name
+    )
+}
