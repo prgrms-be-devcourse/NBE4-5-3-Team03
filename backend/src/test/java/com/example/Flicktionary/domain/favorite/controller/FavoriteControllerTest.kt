4626be9b8b76061a5e787122361a656dab2cb1cc
JunKiBeom <dona0408@gmail.com>
2025-04-09 15:36:53 +0900

    refactor: FavoriteControllerTest - getFavorite() java to kt

--- backend/src/test/java/com/example/Flicktionary/domain/favorite/controller/FavoriteControllerTest.kt
+++ backend/src/test/java/com/example/Flicktionary/domain/favorite/controller/FavoriteControllerTest.kt
@@ -5,17 +5,24 @@ import com.example.Flicktionary.domain.favorite.entity.ContentType
 import com.example.Flicktionary.domain.favorite.service.FavoriteService
 import com.example.Flicktionary.domain.user.service.UserAccountJwtAuthenticationService
 import com.example.Flicktionary.domain.user.service.UserAccountService
+import com.example.Flicktionary.global.dto.PageDto
 import com.example.Flicktionary.global.security.CustomUserDetailsService
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.*
+import org.mockito.kotlin.any
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.given
+import org.mockito.kotlin.then
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.MediaType
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
@@ -110,5 +117,55 @@ class FavoriteControllerTest {
         assertEquals(contentId, captured.contentId)
 
         then(favoriteService).should().createFavorite(any())
+    }
+
+    @Test
+    @DisplayName("즐겨찾기 조회 - 성공")
+    fun getFavorite() {
+        // Given
+        val userId = 1L
+        val longCaptor = argumentCaptor<Long>()
+        val integerCaptor = argumentCaptor<Int>()
+        val stringCaptor = argumentCaptor<String>()
+
+        given(
+            favoriteService.getUserFavorites(
+                longCaptor.capture(),
+                integerCaptor.capture(),
+                integerCaptor.capture(),
+                stringCaptor.capture(),
+                stringCaptor.capture()
+            )
+        ).willReturn(
+            PageDto(
+                PageImpl(
+                    listOf(), PageRequest.of(0, 10), 10
+                )
+            )
+        )
+
+        // When & Then
+        val resultActions = mvc
+            .perform(
+                get("/api/favorites/${userId}")
+                    .contentType(
+                        MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+                    )
+            )
+            .andDo(print())
+
+        resultActions.andExpect(status().isOk())
+
+        val capturedLong = longCaptor.firstValue
+        val capturedIntegers = integerCaptor.allValues
+        val capturedStrings = stringCaptor.allValues
+
+        assertEquals(userId, capturedLong)
+        assertEquals(1, capturedIntegers[0]) // page
+        assertEquals(10, capturedIntegers[1]) // pageSize
+        assertEquals("id", capturedStrings[0]) // sortBy
+        assertEquals("desc", capturedStrings[1]) // direction
+
+        then(favoriteService).should().getUserFavorites(userId, 1, 10, "id", "desc")
     }
 }
